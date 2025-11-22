41432548510d9f72ee82a9970dce042b0205c39e
a <aclghrand@gmail.com>
2025-11-22 00:03:03 -0800

    affix parser

--- src/tli/constants.ts
+++ src/tli/constants.ts
@@ -1,15 +1,18 @@
-export type DmgModType =
-  | "global"
-  | "melee"
-  | "area"
-  | "attack"
-  | "spell"
-  | "physical"
-  | "cold"
-  | "lightning"
-  | "fire"
-  | "erosion"
-  | "elemental";
+export const DMG_MOD_TYPES = [
+  "global",
+  "melee",
+  "area",
+  "attack",
+  "spell",
+  "physical",
+  "cold",
+  "lightning",
+  "fire",
+  "erosion",
+  "elemental",
+] as const;
+
+export type DmgModType = (typeof DMG_MOD_TYPES)[number];
 
 export type CritRatingModType = "global" | "attack" | "spell";
 export type CritDmgModType = "global" | "attack" | "spell";
